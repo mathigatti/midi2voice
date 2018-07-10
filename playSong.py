6c371823dddb8c07436d30e98e13cd7d5cddbd38
Mathias Gatti <mathigatti@gmail.com>
2018-07-10 00:25:24 -0300

    fixes

--- playSong.py
+++ playSong.py
@@ -1,6 +1,8 @@
 from pydub import AudioSegment
 from pydub.playback import play
 import sys
+sys.path.append('.')
+from utils import *
 
 audio2 = AudioSegment.from_file(LAST_INSTRUMENTAL_WAV, parameters=["-vol", "85"])
 audio1 = AudioSegment.from_file(LAST_VOICE_WAV)

4af1c9aa2c57341fb6c605adca436c0f3fa31080
Mathias Gatti <mathigatti@gmail.com>
2021-09-25 21:54:31 -0300

    New version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="midi2voice",
-    version="1.0.1",
+    version="1.0.3",
     author="Mathias Gatti",
     author_email="mathigatti@gmail.com",
     description="Singing synthesis from MIDI file",
